0584f53827d22cc80455c996fdd04b5cb88436ba
Shaheer kayani <shaheer.kayani8@gmail.com>
2025-10-28 15:04:28 -0400

    Initial Commit (incomplete)

--- src/main/java/com/pluralsight/House.java
+++ src/main/java/com/pluralsight/House.java
@@ -1,4 +1,73 @@
 package com.pluralsight;
 
-public class House {
+public class House extends Asset{
+    private String address;
+    private int condition;
+    private int squareFoot;
+    private int lotSize;
+
+    public House(String description, String dateAcquired, double originalCost, String address, int condition, int squareFoot, int lotSize) {
+        super(description, dateAcquired, originalCost);
+        this.address = address;
+        this.condition = condition;
+        this.squareFoot = squareFoot;
+        this.lotSize = lotSize;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getCondition() {
+        return condition;
+    }
+
+    public void setCondition(int condition) {
+        this.condition = condition;
+    }
+
+    public int getSquareFoot() {
+        return squareFoot;
+    }
+
+    public void setSquareFoot(int squareFoot) {
+        this.squareFoot = squareFoot;
+    }
+
+    public int getLotSize() {
+        return lotSize;
+    }
+
+    public void setLotSize(int lotSize) {
+        this.lotSize = lotSize;
+    }
+
+    public double getValue(){
+
+        double priceSquareFeet = 0;
+
+        switch (condition){
+            case 1:
+                priceSquareFeet = 180;
+                break;
+            case 2:
+                priceSquareFeet = 130;
+                break;
+            case 3:
+                priceSquareFeet = 90;
+                break;
+            case 4:
+                priceSquareFeet = 80;
+                break;
+            default:
+                System.out.println("Condition has a wrong value");
+        }
+        return (squareFoot * priceSquareFeet) + (lotSize * 0.25);
+
+    }
+
 }
--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -1,4 +1,49 @@
 package com.pluralsight;
 
-public class Vehicle {
+public class Vehicle extends Asset{
+    private String makeModel;
+    private int year;
+    private int odometer;
+
+    public Vehicle(String description, String dateAcquired, double originalCost, String makeModel, int year, int odometer) {
+        super(description, dateAcquired, originalCost);
+        this.makeModel = makeModel;
+        this.year = year;
+        this.odometer = odometer;
+    }
+
+    public String getMakeModel() {
+        return makeModel;
+    }
+
+    public void setMakeModel(String makeModel) {
+        this.makeModel = makeModel;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getOdometer() {
+        return odometer;
+    }
+
+    public void setOdometer(int odometer) {
+        this.odometer = odometer;
+    }
+
+    public double getValue(){
+
+        double reducedValue = 0;
+/*       if (year <= 3 && year > 0){
+            reducedValue = 0.03;
+        } else if ( ) {
+            
+        }*/
+        return reducedValue = 0;
+    }
 }
